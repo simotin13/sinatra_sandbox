0aecddec88cb727b2d40b4ce5dd9ebae440aff1b
hiroyuki miyazaki <miyazaki@mcommit.com>
2022-11-19 23:16:13 +0900

    Add rspec

---
+++ spec/app_spec.rb
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+ENV['RACK_ENV'] = 'test'
+
+require './myapp.rb'
+require 'rspec'
+require 'rack/test'
+
+describe "MyApp のテスト" do
+  include Rack::Test::Methods
+
+  def app
+    MyApp
+  end
+
+  it "return Hello" do
+    get '/'
+    expect(last_response.status).to eq 200
+    expect(last_response.body).to eq 'Hello'
+  end
+end
+
